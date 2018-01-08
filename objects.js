833de8085192a505b852ac1da9ee310a73b97ad5
James Overbay <orangetoque13@gmail.com>
2018-01-08 04:11:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,5 @@ var playlist = {};
 playlist['Pearl Jam'] = 'Garden';
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, { [artistName]: 'songTitle'});
+  return Object.assign({}, playlist, { [artistName]: songTitle});
 }
