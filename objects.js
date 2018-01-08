bc79687e239fced6ab2e927ea7cf966c04ce37a2
James Overbay <orangetoque13@gmail.com>
2018-01-08 04:15:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { [artistName]: songTitle});
 }
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistname;
-  return playlist
+  delete playlist.artistName;
+  return playlist;
 }
